58baab1686bab8fd5190a402f4593a89f1353d36
panqian <455234037@qq.com>
2016-10-24 15:45:47 +0800

    2016.10.24 js改成cdn链接

---
+++ src/components/footer/myFooter.vue
@@ -0,0 +1,21 @@
+<template>
+	<footer>
+		<hr />
+		<p>© To the oak tree 2016</p>
+	</footer>
+</template>
+
+<script>
+export default {
+  name: 'myFooter'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+	footer{
+		margin-left:auto; 
+		margin-right:auto;
+		width:90%;
+	}
+</style>
---
+++ src/components/header/myNav.vue
@@ -0,0 +1,45 @@
+<template>
+  <header id="myNav">
+    <nav class="navbar navbar-static-top navbar-dark bg-inverse">
+      <img class="navbar-brand" src="~assets/img/oak_logo.png" style=""></img>
+      <!--<a class="navbar-brand" href="#"></a>-->
+      <ul class="nav navbar-nav">
+        <li class="nav-item active">
+          <router-link :to="{ name: 'routeruser',params: { id: 123 } }" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'myNav',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  nav{
+    background-color: transparent!important;
+  }
+  img{
+    height: 3rem;
+    margin-top:-0.325rem;
+    margin-left:7rem;
+  }
+  .navbar-nav .nav-link{
+    padding-left: 0.725rem;
+  }
+</style>
